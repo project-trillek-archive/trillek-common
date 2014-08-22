bd8e2172900c71c28a89404f504db12f22d71b33
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-08-22 05:37:15 +0000

    Rocket event registration

--- include/systems/gui.hpp
+++ include/systems/gui.hpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <Rocket/Core/SystemInterface.h>
 #include <Rocket/Core/FileInterface.h>
+#include <Rocket/Core/EventListener.h>
+#include <Rocket/Core/EventListenerInstancer.h>
 #include <Rocket/Core/Context.h>
 #include <Rocket/Core/ElementDocument.h>
 
@@ -34,6 +36,7 @@ public:
 };
 typedef std::unique_ptr<Rocket::Core::ElementDocument, DocumentUnloader> unique_doc_ptr;
 class GuiSystem : public Rocket::Core::SystemInterface,
+    public Rocket::Core::EventListener,
     public event::Subscriber<KeyboardEvent>,
     public event::Subscriber<MouseBtnEvent>,
     public event::Subscriber<MouseMoveEvent> {
@@ -46,6 +49,8 @@ public:
     virtual float GetElapsedTime();
     virtual bool LogMessage(Rocket::Core::Log::Type type, const Rocket::Core::String& message);
 
+    virtual void ProcessEvent(Rocket::Core::Event& event);
+
     void Start();
     void LoadDocument(const std::string &);
     void LoadFont(const std::string &);
@@ -53,10 +58,32 @@ public:
     void Notify(const KeyboardEvent*);
     void Notify(const MouseBtnEvent*);
     void Notify(const MouseMoveEvent*);
+
+    class GuiInstancer : public Rocket::Core::EventListenerInstancer {
+    public:
+        GuiInstancer(GuiSystem &u) : gs(u) {}
+        virtual ~GuiInstancer() {}
+        virtual Rocket::Core::EventListener* InstanceEventListener(const Rocket::Core::String& value, Rocket::Core::Element* element);
+        virtual void Release();
+    private:
+        GuiSystem &gs;
+    };
+    class GuiEventInterface : public Rocket::Core::EventListener {
+    public:
+        GuiEventInterface(GuiSystem &u, uint32_t id);
+        virtual ~GuiEventInterface();
+        virtual void ProcessEvent(Rocket::Core::Event& event);
+    private:
+        uint32_t instance_id;
+        GuiSystem &gs;
+    };
 private:
     void Update();
     void InvokeRender();
 
+    std::unique_ptr<GuiInstancer> instancer;
+    uint32_t instance_id;
+    std::list<std::unique_ptr<GuiEventInterface>> event_listeners;
     OS &opsystem;
     graphics::RenderSystem &grsystem;
     std::unique_ptr<Rocket::Core::Context, ReferenceDeleter> main_context;
--- src/systems/gui.cpp
+++ src/systems/gui.cpp
@@ -53,6 +53,38 @@ void GuiSystem::Notify(const MouseBtnEvent* mouse_event) {
 void GuiSystem::Notify(const MouseMoveEvent* mouse_event) {
     this->main_context->ProcessMouseMove(mouse_event->new_x, mouse_event->new_y, 0);
 }
+void GuiSystem::ProcessEvent(Rocket::Core::Event& event) {
+    Rocket::Core::Element* te = event.GetTargetElement();
+    Rocket::Core::String idstring;
+    if(te) {
+        idstring = te->GetId();
+    }
+    LOGMSGC(NOTICE) << idstring.CString() << " Got event " << event.GetType().CString();
+}
+Rocket::Core::EventListener* GuiSystem::GuiInstancer::InstanceEventListener(
+        const Rocket::Core::String& value, Rocket::Core::Element* element) {
+    LOGMSGC(NOTICE) << "Register event " << element->GetTagName().CString() << ":" << value.CString();
+    GuiEventInterface *ev = new GuiEventInterface(this->gs, ++this->gs.instance_id);
+    this->gs.event_listeners.push_back(std::unique_ptr<GuiEventInterface>(ev));
+    return ev;
+}
+void GuiSystem::GuiInstancer::Release() {
+    LOGMSGC(NOTICE) << "Unregister event";
+}
+GuiSystem::GuiEventInterface::GuiEventInterface(GuiSystem &u, uint32_t id) :
+    gs(u), instance_id(id) {
+}
+GuiSystem::GuiEventInterface::~GuiEventInterface() {
+
+}
+void GuiSystem::GuiEventInterface::ProcessEvent(Rocket::Core::Event& event) {
+    Rocket::Core::Element* te = event.GetTargetElement();
+    Rocket::Core::String idstring;
+    if(te) {
+        idstring = te->GetId();
+    }
+    LOGMSGC(NOTICE) << idstring.CString() << " Got custom [" << instance_id << "] event " << event.GetType().CString();
+}
 
 void GuiSystem::Update() {
     this->main_context->Update();
@@ -64,6 +96,8 @@ void GuiSystem::LoadDocument(const std::string &docname) {
     Rocket::Core::ElementDocument *elemdoc = this->main_context->LoadDocument(docname.c_str());
     if(elemdoc != nullptr) {
         elemdoc->Show();
+        elemdoc->AddEventListener(Rocket::Core::String("click"), this, true);
+
         this->documents.push_back(unique_doc_ptr(elemdoc));
     } else {
         return; // failed
@@ -77,8 +111,12 @@ void GuiSystem::Start() {
     Rocket::Core::SetRenderInterface(this->grsystem.GetGUIInterface());
     Rocket::Core::Initialise();
     Rocket::Controls::Initialise();
+    instancer.reset(new GuiInstancer(*this));
+    Rocket::Core::Factory::RegisterEventListenerInstancer(instancer.get());
 
-    this->main_context.reset(Rocket::Core::CreateContext("default", Rocket::Core::Vector2i(1024, 768)));
+    this->main_context.reset(Rocket::Core::CreateContext(
+        "default", Rocket::Core::Vector2i(this->opsystem.GetWindowWidth(), this->opsystem.GetWindowHeight())
+    ));
     event::Dispatcher<KeyboardEvent>::GetInstance()->Subscribe(this);
     event::Dispatcher<MouseBtnEvent>::GetInstance()->Subscribe(this);
     event::Dispatcher<MouseMoveEvent>::GetInstance()->Subscribe(this);
